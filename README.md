90f0d46efcf3b8dd308560b2c72c71c88f641d5a
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-26 23:03:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-throttle-map-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-9gwhq2)
---
+++ index.ts
@@ -0,0 +1,22 @@
+import { interval } from 'rxjs';
+import { throttle, map } from 'rxjs/operators';
+
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/throttle
+
+// Example 2: Throttle with promise
+//emit value every 1 second
+const source = interval(1000);
+//incrementally increase the time to resolve based on source
+const promise = val =>
+  new Promise(resolve =>
+    setTimeout(() => resolve(`Resolved: ${val}`), val * 100)
+  );
+//when promise resolves emit item from source
+const example = source
+  .pipe(
+    throttle(promise),
+    map(val => `Throttled off Promise: ${val}`)
+  );
+
+
+const subscribe = example.subscribe(val => console.log(val));
